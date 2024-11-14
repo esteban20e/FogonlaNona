3557fb48609da4abbdb08c6013b731025d5bd0d6
Cristian3016 <cristiancaro163@gmail.com>
2024-11-14 17:30:33 -0300

    Delete

--- backend/src/routes/orderRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrder, getOrder } from "../controllers/orderController";
-
-const router = Router();
-
-router.post("/", createOrder);
-router.get("/:id", getOrder);
-
-export default router;
--- backend/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, getUserOrders } from "../controllers/userController";
-
-const router = Router();
-
-router.post("/", createUser);
-router.get("/:id/orders", getUserOrders);
-
-export default router;
